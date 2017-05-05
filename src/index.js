92084b6223853c0b22706c38bc80c55675a5f5de
Skotiniotis <therapon@amazon.com>
2017-05-05 12:30:38 -0700

    feat(greet): Appends string to the end of the greet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 var uniqueArray = require('unique-random-array');
 var greetingsStrings = require('./greetings.json'); 
+var getRandomGreeting = uniqueArray(greetingsStrings);
 
 module.exports = { 
     greetings: greetingsStrings,
-    greet: uniqueArray(greetingsStrings)
+    greet: uniqueArray(greetingsStrings),
+    greetWithSuffix: greetSuffix
 
-}; 
+};
+
+function greetSuffix(suffix) { 
+    var tmp = getRandomGreeting();
+    return tmp + ', ' + suffix; 
+} 
